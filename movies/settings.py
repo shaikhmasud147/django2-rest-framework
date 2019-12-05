dde985d7e7f3fd68062b4c7ba00748419069fa3d
Masud Shaikh <masud@codism.io>
2019-12-05 13:45:18 +0530

    Production changes

--- movies/settings.py
+++ movies/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-v&^n=ry5q=s%q5j@l0^4sk5(f+(30p4@(dxxjc&__e!b4=ekk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
