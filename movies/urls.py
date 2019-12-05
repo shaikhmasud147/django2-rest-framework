b98b805b1b6d4db6311eadaf486b5cb10b3ae52b
Masud Shaikh <masud@codism.io>
2019-12-05 15:29:28 +0530

    Production changes

--- movies/urls.py
+++ movies/urls.py
@@ -14,8 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
 
@@ -24,7 +22,3 @@ urlpatterns = [
     path('api/', include('imdbapi.urls')),
     path('auth/', obtain_auth_token),
 ]
-
-urlpatterns = [
-    # ... the rest of your URLconf goes here ...
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
